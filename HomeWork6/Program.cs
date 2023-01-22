196f777d46b38674af6ae12b5cfdbcb51ca11b2e
Andrey <andreytelushko@gmail.com>
2023-01-22 11:02:25 +0300

    Lesson 41

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -8,8 +8,24 @@ namespace HomeWork6
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Введите числа через запятую, без пробелов. \nПример: 1,2,3,4 \n");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Чисел больше нуля: {Counter(input)}");
+        }
+
+        public static int Counter(string input)
+        {
+            var array = input.Split(',');
+
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (int.Parse(array[i]) > 0) result++;
+            }
+
+            return result;
         }
     }
 }
